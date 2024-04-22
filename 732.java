10ce056c36123b123c4ad2306e387db7aa9c42b4
Humberto Corrêa Gomes <humberto.gomes002@edu.pucrs.br>
2024-04-22 11:44:13 -0300

    new leetcode problems

---
+++ 732.java
@@ -0,0 +1,24 @@
+import java.util.TreeMap;
+
+class MyCalendarThree {
+    TreeMap<Integer,Integer> timeline;
+    public MyCalendarThree() {
+        timeline = new TreeMap<>();
+    }
+    
+    public int book(int start, int end) {
+        timeline.put(start,timeline.getOrDefault(start,0)+1);
+        timeline.put(end,timeline.getOrDefault(end,0)-1);
+        
+        int ongoing=0, k=0;
+        for(int v:timeline.values())
+            k = Math.max(k,ongoing+=v);
+        return k;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(startTime,endTime);
+ */
