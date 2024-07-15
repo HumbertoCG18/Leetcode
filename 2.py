e90f6cd3decebf46d602563d641a180c9a3345ee
Humberto Corrêa Gomes <humberto.cgomes17@gmail.com>
2024-07-15 18:57:09 -0300

    update

---
+++ 2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy = ListNode()
+        res = dummy
+
+        total = carry = 0
+
+        while l1 or l2 or carry:
+            total = carry
+
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            
+            num = total % 10
+            carry = total // 10
+            dummy.next = ListNode(num)
+            dummy = dummy.next
+        
+        return res.next
